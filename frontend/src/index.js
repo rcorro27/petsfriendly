12291cac2287d76953a700d774ad55f4ce85d9dd
richard <rcorroch@isimtl.local>
2020-07-21 11:03:23 -0400

    ajouts des pages html et js

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import FormContainer from 'container/form-container' A CHANGER
+import MusicContainer from './container/music-container'
+
+ReactDOM.render(
+    <MusicContainer />,
+    document.getElementById('app')
+)
