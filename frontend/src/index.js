3ff661103982173c5f3780571738c2b8650b6f36
richard <rcorroch@isimtl.local>
2020-07-27 22:24:52 -0400

    conflit regles le deux changement garder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-<<<<<<< HEAD
 import FormContainer from 'container/form-inscription'
-=======
 import InputInscription from 'container/form-inscription'
->>>>>>> master
 
 ReactDOM.render(
     <InputInscription />,
