9af15d95e42ec3560d78e37f8da6e6b4ccbedda6
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-07-28 10:51:30 -0400

    ajout

--- android/PetSitterISI/app/src/main/java/com/example/petsitterisi/Connexion.java
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/Connexion.java
@@ -2,12 +2,6 @@ package com.example.petsitterisi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
-import android.os.Bundle;
-
-public class Connexion extends AppCompatActivity {
-
-=======
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
@@ -19,19 +13,15 @@ public class Connexion extends AppCompatActivity {
     Button connexion_button;
     Context ctx;
 
->>>>>>> mobile_android
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
-<<<<<<< HEAD
-=======
 
         top_textView = findViewById(R.id.top_textView);
         connexion_button = findViewById(R.id.connexion_button);
         top_textView.setBackground(getResources().getDrawable(R.drawable.rounded_corner));
         connexion_button.setBackgroundColor(getResources().getColor(R.color.black));
         connexion_button.setTextColor(getResources().getColor(R.color.white));
->>>>>>> mobile_android
     }
 }
