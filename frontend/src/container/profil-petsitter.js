763de7b8f30db91bd37e8fa9e586598923a3ab69
ahmed <ahmed.meksi28@gmail.com>
2020-07-28 09:47:56 -0400

    correction inscription et profil petSitter and sitter

---
+++ frontend/src/container/profil-petsitter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import InputComponent from 'component/input-component'
+
+class profilPettSitter extends Component {
+    render () {
+        return (
+            // AHMED CHAQUE ELEMENT JSX DOIT AVOIR UNE ELEMENT PARENT ( ce ca le div qui envelope tout le restes)
+            // ligne 21 pas la bonne syntaxe
+            <div>
+
+                <div>
+                    <h1>Contacter le PetSitter</h1>
+
+                    <h2>Laisser lui un messager</h2>
+                </div>
+
+                <form id='petSitterpopups'>
+                    <p>Message</p>
+                    <div class='texteArea'>
+                        <InputComponent text='pettesitter' type='TEXTAREA' id='message' name='message' value='message' />
+
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+// tu ne exporte pas la bon classe!!! tu dois exporter la classe container dans ce cas la : FormInscription!!
+export default profilPettSitter
