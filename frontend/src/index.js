f3ae570c0618c924c0e67366fa7fe5ccf29a33eb
richard <rcorroch@isimtl.local>
2020-07-28 23:03:42 -0400

    inde regler ne plus toucher

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-
+import FormContainer from 'container/form-container'
+ReactDOM.render(
     <FormContainer />,
     document.getElementById('app')
 )
