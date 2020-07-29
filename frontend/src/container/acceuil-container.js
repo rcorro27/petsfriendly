24dcee69f2dfabd141767987e3650536dc9b0383
Yahia Benhaili <ybenhail@isimtl.local>
2020-07-29 09:14:43 -0400

    Caroussel

--- frontend/src/container/acceuil-container.js
+++ frontend/src/container/acceuil-container.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react'
 
-import Navbar from './navbar-container'
-import Footer from '../component/Footer/Footer'
 // import SliderItems from '../component/sliders/SliderItems'
-import Title from '../component/titre-composant'
-import SpanComponent from '../component/span-composant'
-import CarouselContainer from '../container/carousel-container'
 
 // import Navbar from '../component/Navbar'
 
 class FormContainer extends Component {
     render () {
         return (
-
-            <div>
-                <Navbar />
-                <div />
-                <main id='divContainer'>
-                    <CarouselContainer />
-                    <Title
-                        titre1='titre 1Nous considerons votre Animal'
-                        titre2='Comme un membre De notre Famille'
-                    />
-                    <SpanComponent divClass='test' text='Yahia' />
-                </main>
-                <Footer />
-            </div>
+            <div />
 
         )
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-import FormContainer from 'container/acceuil-container'
+import PageAcceuil from 'container/page-acceuil-container'
 
 ReactDOM.render(
 
-    <FormContainer />,
+    <PageAcceuil />,
     document.getElementById('app')
 )
