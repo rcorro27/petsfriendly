1f82bce772409bd0e6531e1fee442b9f130a6aae
richard <rcorroch@isimtl.local>
2020-07-29 10:06:05 -0400

    pages origin efface , form container change

--- frontend/src/container/form-container.js
+++ frontend/src/container/form-container.js
@@ -1,40 +1,42 @@
 import React, { Component } from 'react'
 
 import Footer from '../component/Footer/Footer'
-import Slider from '../component/sliders/Slider'
-import Title from '../component/Titles/Title'
-import DivSpans from '../component/Buttons/DivSpans'
+import CarouselContainer from '../container/carousel-container'
+import Title from '../component/titre-composant'
+import SpanComponent from '../component/span-composant'
 import InputComponent from '../component/input-component'
-// import Navbar from '../component/Navbar'
+import Navbar from '../container/navbar-container'
 
 class FormContainer extends Component {
     render () {
         return (
 
             <div>
-                <Navbar />
+
                 <div>
-                <InputComponent
-                    text='Nom :'
-                    type='text'
-                    id='nom'
-                    name='nom'
-                />
-                <InputComponent
-                    text='Nom :'
-                    type='text'
-                    id='nom'
-                    name='nom'
-                />
-                        />
-                    </form>
+                    <Navbar />
+                    <InputComponent
+                        text='Nom :'
+                        type='text'
+                        id='nom'
+                        name='nom'
+                    />
+                    <InputComponent
+                        text='Nom :'
+                        type='text'
+                        id='nom'
+                        name='nom'
+                    />
+
                     <button onClick={this.props.onHandleSaveOnClick}>retour a la page</button>
-                <main id='divContainer'>
-                    <Slider />
-                    <Title titre1='titre 1 ' titre2='titre 2 ' />
-                    <DivSpans divClass='test' />
-                </main>
-                <Footer />
+                    <main id='divContainer'>
+                        <CarouselContainer />
+                        <Title titre1='titre 1 ' titre2='titre 2 ' />
+                        <SpanComponent divClass='test' text='test ' />
+                    </main>
+                    <Footer />
+                </div>
+
             </div>
 
         )
