19bfa1053a9be2940eab9e98116ffc8d69a3decc
richard <rcorroch@isimtl.local>
2020-07-29 10:51:31 -0400

    pages devellopement

---
+++ frontend/src/container/developpement-container.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+// import ToolbarComponent from 'component/toolbar-component'
+// import FormContainer from 'container/form-container'
+// import RecherchePetsitter from 'container/recherchepetsitter-container'
+class DeveloppementContainer extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Page DEVELOPEMENT</h1>
+
+                <div>
+                    <button onClick={this.handleOnClickRichard}>Richard</button>
+                    <button onClick={this.handleOnClickAhmed}>Ahmed</button>
+                    <button onClick={this.handleOnClickYahia}>Yahia</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DeveloppementContainer
