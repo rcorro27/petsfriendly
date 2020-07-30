48e8a25c6108af6bc123840ac2c9eea1cfa4d183
Kodjo Michel Adjognon <kadjogno@isimtl.local>
2020-07-30 13:04:36 -0400

    correction des fragment

---
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/Recherche_service_3.java
@@ -0,0 +1,14 @@
+package com.example.petsitterisi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recherche_service_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche_service_3);
+    }
+}
