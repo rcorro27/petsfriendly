bd68144dfa76e23b5a89f7efe42fac3d0e07c100
Yahia Benhaili <ybenhail@isi-mtl.com>
2020-07-30 12:46:12 -0400

    connecction form

--- frontend/src/component/navbar-links.js
+++ frontend/src/component/navbar-links.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import ConnectionPopUp from './popup-connection'
 
 function NavbarLinks ({ param1, param2 }) {
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
     return (
         <div className='collapse navbar-collapse' id='navbarResponsive'>
             <ul className='navbar-nav ml-auto'>
                 <li className='nav-item active'>
-                    <a className='nav-link' href='test'>{param1}
-                    </a>
+                    <Button variant='light mx-2' onClick={handleShow}>{param1}</Button>
                 </li>
                 <li className='nav-item'>
-                    <a className='nav-link' href='test'>{param2}</a>
+                    <Button variant='light'>{param2}</Button>
                 </li>
 
             </ul>
-        </div>
 
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Page Connexion</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ConnectionPopUp />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={handleClose}>
+                        Annuler
+                    </Button>
+                    <Button variant='primary' onClick={handleClose}>
+                        Se connecter
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     )
 }
 
---
+++ frontend/src/component/popup-connection.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+
+function ConnectionPopUp () {
+    const [show, setShow] = useState(false)
+
+    // const handleClose = () => setShow(false)
+    return (
+        <div class='modal-body mx-3'>
+            <div class='md-form mb-5'>
+                <i class='fas fa-envelope prefix grey-text' />
+                <input type='email' id='defaultForm-email' class='form-control validate' />
+                <label data-error='wrong' data-success='right' for='defaultForm-email'>Ton Email</label>
+
+            </div>
+            <div class='md-form mb-4'>
+                <i class='fas fa-lock prefix grey-text' />
+                <input type='password' id='defaultForm-pass' class='form-control validate' />
+                <label data-error='wrong' data-success='right' for='defaultForm-pass'>Ton mot de passe</label>
+            </div>
+        </div>
+    )
+}
+
+export default ConnectionPopUp
