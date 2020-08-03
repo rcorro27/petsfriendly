8fa315e52f761d2600f754299d1766a69a647724
ahmed <ahmed.meksi28@gmail.com>
2020-08-03 13:04:03 -0400

    upload image

--- frontend/src/container/profil-proprietair-containaire.js
+++ frontend/src/container/profil-proprietair-containaire.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-
 import InputComponent from 'component/input-component'
 import Boutton from 'component/bouton-component'
+import ImageUploader from 'react-images-upload'
 class ProfilProprietaireBord extends Component {
+    constructor (props) {
+        super(props)
+        this.state = { pictures: [] }
+        this.onDrop = this.handleOnDrop.bind(this)
+    }
+
     render () {
         return (
             <div>
-                <div class='imageProfil'>
-                    <InputComponent text='changer votre photo de profil' type='file' id='img' name='imgpetteSitter' />
-                </div>
+                <div class='imageProfil' />
+
+                handleOnDrop(picture) {
+                    this.setState({
+                        pictures: this.state.pictures.concat()
+                    })
+                }
+
+                <ImageUploader
+                    withIcon
+                    buttonText='Choose images'
+
+                    imgExtension={['.jpg', '.gif', '.png', '.gif']}
+
+                />
+
                 <div>
                     <h1>Mes Information </h1>
 
@@ -44,6 +63,7 @@ class ProfilProprietaireBord extends Component {
                             <InputComponent text='date debut' type='date' id='date_debut' name='dateD' />
                             <InputComponent text='date fin' type='date' id='date_debut' name='datef' />
                         </div>
+
                         <div class='ajout-animal'>
                             <Boutton type='submit' id='ajout' name='envoi' value='ajouter' />
 
