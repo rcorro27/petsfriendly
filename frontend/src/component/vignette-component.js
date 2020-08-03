11d9c90aa4a4535740acd5e6c6073517d7190422
richard <rcorroch@isimtl.local>
2020-08-03 10:34:34 -0400

    rapport journalier richard

--- frontend/src/component/vignette-component.js
+++ frontend/src/component/vignette-component.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import imgComposant from 'component-img-composant'
+// import imgComposant from 'img-composant'
 
 /* Json {
 "petsitter1" : {
@@ -26,13 +26,16 @@ import imgComposant from 'component-img-composant'
 }
 } */
 
-const ulComponent = (name, text, codepostal, button) => {
+// EVENEMENT BUTTON A AJOTER LIGNE 36
+
+function ulComponent (name, text, codepostal, button) {
     return (
 
         <div>
             <ol>
                 <li>{text}</li><button onClick={button.handleOnClickAimer}><i class='fa fa-heart' aria-hidden='true' /></button>
                 <p>{codepostal}</p>
+                <p>{name}</p>
 
             </ol>
 
@@ -40,15 +43,29 @@ const ulComponent = (name, text, codepostal, button) => {
     )
 }
 
-const VignetteComponent = ({ pettSitters, classname }) => (
+const VignetteComponent = ({ urlPhoto, classname, name, text, codepostal }) => (
     // est ce que on recoit une liste de pettsitter ?? question a posser
-
+/* key={index} */
     <div>
-        {pettSitters.map((info, index) => <imgComposant srcIMG={info.url_photo} key={index} classBo={classname} />)}
+        {/* <imgComposant srcIMG={urlPhoto} classBo={classname} /> */}
+        <img className={classname} src={urlPhoto} alt={urlPhoto} />
 
-        {pettSitters.map((info, index) => ulComponent(info.nom, info.textDescriptif, index))}
+        <ulComponent name={name} text={text} codepostal={codepostal} />
 
     </div>
+
 )
+/*
+function Title ({ id, titre1, titre2 }) {
+    return (
+        <div id={id}>
+            <p> {titre1} </p>
+            <p> {titre2} </p>
+            <hr />
+        </div>
+
+    )
+}
+*/
 export default VignetteComponent
 // {buttons.map((button, index) => <span key={index}><button onClick={button.handleOnClick}>{button.label}</button></span>)}
--- frontend/src/container/resultat-recherche-pett-sitter-container.js
+++ frontend/src/container/resultat-recherche-pett-sitter-container.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import ListItemComponent from 'component/list-item-component'
+import VignetteComponent from 'component/vignette-component'
 class ResultatRecherchePetsitter extends Component {
     render () {
         return (
             <div>
                 <ListItemComponent text='RECHERCHE' />
+                {/* const VignetteComponent = ({ urlPhoto, classname, name, text, codepostal }) */}
+                <VignetteComponent urlPhoto='frontend/src/img/caroussel/image1.jpeg' classname='toto' name='test name' text='titre normalement' codePostal='h33h3h3' />
                 <button onClick={this.props.onHandleSaveOnClick}>retour recherche</button>
 
             </div>
