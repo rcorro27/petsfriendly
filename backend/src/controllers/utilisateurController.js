1a7ebcc7ef5b7f541f4c2cdacbd1951a8293e203
Nassim Arib <narib@isi-mtl.com>
2020-08-05 18:22:06 -0400

    connexion utilisateur

--- backend/src/controllers/utilisateurController.js
+++ backend/src/controllers/utilisateurController.js
@@ -1,9 +1,28 @@
-
+const bd = require('../servers/bd')
+const {Utilisateur} = require('../models/utilisateur')
 
 //la fonction appelee par la route connexion d'utilisateur
 function utilisateurConnexion(req, res)
 {
+    let sql = "SELECT * FROM utilisateur WHERE email=$1 and mot_de_passe=$2" //la req sql  executer
 
+    /* a regler demain matin*/
+    //let valeursRequetes = bd.transformerReqParamsAUnTableau(req.body) //pour transformer le body de la requete a un tableau
+    //console.log(valeursRequetes)
+    bd.excuterRequete(sql, ['rufin@nassim.com', 'abc123...']) //executer la req sql
+
+    .then(resultatRequete => { 
+        //si la requete s'est bien passe on recoit le resultat et on l'envoie dans la reponse
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(resultatRequete.rows))
+    })
+    .catch(erreur => {
+        console.error(erreur.stack)
+
+        //en cas d'erreur on l'envoie pour l'instant
+        res.setHeader('Content-Type', 'text/html');
+        res.end(erreur.stack)
+    })
 }
 
 //la fonction appelee par la route creation d'utilisateur
