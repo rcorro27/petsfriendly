8b3c7d8fe793e8291cc6691b7ebef865085fe14d
kelfilal <kelfilal@isimtl.local>
2020-08-11 11:42:17 -0400

    profilPetSitterManager

--- android/PetSitterISI/app/src/main/java/com/example/petsitterisi/managers/ProfilSitterManager.java
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/managers/ProfilSitterManager.java
@@ -1,8 +1,32 @@
 package com.example.petsitterisi.managers;
+import android.content.Context;
+import android.widget.TextView;
+
+import com.example.petsitterisi.services.ApiUtilisateurFetcher;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 public class ProfilSitterManager {
 
 
+    public static void getUtilisateur(Context context, String mail, String mot_de_passe, TextView error) {
 
+        //creation du Json
+        JSONObject connexionJson = new JSONObject();
+        try {
+            connexionJson.put("email", mail);
+            connexionJson.put("mot_de_passe",  mot_de_passe);
+            //connexion a l'Api
+
+            ApiUtilisateurFetcher apiFetcher = new ApiUtilisateurFetcher(context, error);
+
+            apiFetcher.execute("https://pets-friendly.herokuapp.com/utilisateurs/recuperation/2", connexionJson.toString());
+
+        }catch (JSONException e) {
+        e.printStackTrace();
+    }
+
+    }
 
 
 
