c132438278a2e53ac376e2928fde4dd9d9612c8a
Kodjo Michel Adjognon <kadjogno@isimtl.local>
2020-08-12 18:40:28 -0400

    ajouter favoris manager

--- android/PetSitterISI/app/src/main/java/com/example/petsitterisi/ListePetSitter.java
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/ListePetSitter.java
@@ -10,6 +10,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.petsitterisi.services.ApiListPetSitterFetcher;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,61 +35,11 @@ public class ListePetSitter extends Activity {
         setContentView(R.layout.activity_liste_pet_sitter);
         ll = findViewById(R.id.card_container);
 
+        ApiListPetSitterFetcher apiListPetsitter = new ApiListPetSitterFetcher(ctx, ll);
 
-getJson(ll);
+        apiListPetsitter.execute();
+
     }
-
-
-
-    public void getJson (LinearLayout llParam) {
-        String tContents = "";
-        String concat = "";
-        try {
-            InputStream stream = getAssets().open("resultat-recherche.json");
-            int size = stream.available();
-            byte[] buffer = new byte[size];
-            stream.read(buffer);
-            stream.close();
-            tContents = new String(buffer);
-
-            JSONObject obj = new JSONObject(tContents);
-            JSONArray jsonArray = new JSONArray();
-            jsonArray.put(obj);
-
-            for(int i = 0; i < jsonArray.length(); i++){
-                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                 Iterator<String> itr = jsonObject.keys();
-
-                 while(itr.hasNext()){
-                     String key = itr.next();
-                     JSONArray newJsonArray = jsonObject.getJSONArray(key);
-                     for(int j = 0; j < newJsonArray.length(); j++) {
-                        JSONObject jsObject = newJsonArray.getJSONObject(j);
-
-                         View cardPetSitterParam = View.inflate(ctx , R.layout.card_pet_sitter,null);
-                         TextView petSitterName = cardPetSitterParam.findViewById(R.id.name);
-                         petSitterName.setText(jsObject.getString("nom"));
-                         llParam.addView(cardPetSitterParam);
-
-
-                     }
-
-                 }
-
-            }
-
-
-
-
-        } catch (IOException | JSONException e) {
-            // Handle exceptions here
-
-        }
-    }
-
-
-
-
 
 
 
---
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/services/ApiServicesFetcher.java
@@ -0,0 +1,148 @@
+package com.example.petsitterisi.services;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.petsitterisi.BottomNavigationBar;
+import com.example.petsitterisi.managers.UtilisateurManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+
+public class ApiServicesFetcher extends AsyncTask<String, Nullable, String> {
+
+    private Context  context;
+    TextView error;
+    String email;
+    String mot_de_passe;
+
+
+    public ApiServicesFetcher(Context  context, TextView error, String email, String mot_de_passe) {
+        this.context = context;
+        this.email = email;
+        this.mot_de_passe = mot_de_passe;
+        this.error = error;
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+        String result = "";
+
+        try {
+            URL url = new URL(urls[0]);
+
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.connect();
+
+            JSONObject connexionJson = new JSONObject();
+
+                connexionJson.put("email", email);
+                connexionJson.put("mot_de_passe",  mot_de_passe);
+
+            DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+            wr.writeBytes(connexionJson.toString());
+            wr.flush();
+            wr.close();
+
+            int codeRetour = urlConnection.getResponseCode();
+
+            if (codeRetour == HttpURLConnection.HTTP_OK) {
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+              String line = "";
+                while ((line = in.readLine()) != null)
+                    result += line;
+
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        try {
+            JSONObject jsonObjectDuServeur = new JSONObject(s);
+
+            //Recuperateion des nom des enfants JsonObject
+            Iterator<String> itr = jsonObjectDuServeur.keys();
+            while(itr.hasNext()) {
+                String key = itr.next();
+
+                if(key.equals("utilisateur")){
+
+                    JSONObject utilisateurJson = jsonObjectDuServeur.getJSONObject(key);
+                    String id = utilisateurJson.getString("id");
+
+                    UtilisateurManager.addIdUtilisateur(context, Integer.parseInt(id));
+
+                    Intent intent = new Intent(context, BottomNavigationBar.class);
+                    context.startActivity(intent);
+
+                }else if(key.equals("erreur")){
+                    error.setText("Erreur d'email et de mot passe");
+                }
+
+                //l'object json
+
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private String inputStreamToString(InputStream is) {
+        String rLine = "";
+        StringBuilder answer = new StringBuilder();
+
+        InputStreamReader isr = new InputStreamReader(is);
+
+        BufferedReader rd = new BufferedReader(isr);
+
+        String in = "";
+
+
+        try {
+            while ((rLine = rd.readLine()) != null) {
+                answer.append(rLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return answer.toString();
+    }
+
+
+}
