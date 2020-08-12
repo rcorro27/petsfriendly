67a4276aed91eb5b33b8aea622fc4560d61f89d1
Richard Alexander Corro Chirinos <rcorroch@isimtl.local>
2020-08-12 12:23:04 -0400

    fichier json changer reglements dans vignette component

--- frontend/dictionaire.js
+++ frontend/dictionaire.js
@@ -1,1 +1,1 @@
-const price = {}
+
--- frontend/src/component/vignette-component.js
+++ frontend/src/component/vignette-component.js
@@ -1,45 +1,17 @@
 import React from 'react'
 //* import ImgComposant from 'img-composant'
 
-/* Json {
-"petsitter1" : {
-"id": ,
-"nom": ,
-"prenom": ,
-"age": ,
-"sexe": ,
-"id_adresse": ,
-"telephone": ,
-"secteur_action": ,
-"url_photo": ,
-}
-"petsitter2" : {
-"id": ,
-"nom": ,
-"prenom": ,
-"age": ,
-"sexe": ,
-"id_adresse": ,
-"telephone": ,
-"secteur_action": ,
-"url_photo": ,
-}
-} */
-
-// EVENEMENT BUTTON A AJOTER LIGNE 36
-
-const VignetteComponent = ({ urlPhoto, className, nom, secteurAction, onClickProfil, classInput, classInput2, onClickEnvoyer, textBoutonProfil, textBoutonEnvoyer, services }) => (
+const VignetteComponent = ({ urlPhoto, className, nom, rating, onClickProfil, classInput, classInput2, onClickEnvoyer, textBoutonProfil, textBoutonEnvoyer, servicesSitter, services }) => (
 
     <div className={className}>
         <img className='img-fluid img-thumbnail' src={urlPhoto} alt={urlPhoto} />
         <h2 className='mx-auto'>{nom}</h2>
         {console.log(services)}
-        <ul>
-
-            {services.map((infos, index) => <li key={index}> {infos.service}{''}{infos.price}{''}<i className='fas fa-dollar-sign' /></li>)}
-
+        <ul className='list-group'>
+            {/* ul  a sortir dans une autre function */}
+            {servicesSitter.map((infos, index) => <li key={index} className='list-group-item list-group-item-danger'> {infos}<p>{infos}<i className='fas fa-dollar-sign' /></p></li>)}
         </ul>
-        <p className='mx-auto'>{secteurAction}</p>
+        <p className='mx-auto'>{rating}</p>
         <button type='button' onClick={onClickProfil} className={classInput}>{textBoutonProfil} </button>
         <button type='button' onClick={onClickEnvoyer} className={classInput2}>{textBoutonEnvoyer}</button>
 
