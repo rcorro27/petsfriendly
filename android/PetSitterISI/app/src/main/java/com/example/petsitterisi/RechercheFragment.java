3c3a91ebaca885f7ad1d866663288e5287909ddd
Kodjo Michel Adjognon <kadjogno@isimtl.local>
2020-08-12 11:50:15 -0400

    ajouter contrat manager mobile

--- android/PetSitterISI/app/src/main/java/com/example/petsitterisi/RechercheFragment.java
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/RechercheFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -20,6 +21,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.petsitterisi.services.ApiRechercheFetcher;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import java.io.InputStream;
 import java.util.Calendar;
@@ -29,9 +31,28 @@ public class RechercheFragment extends Fragment {
     DatePickerDialog picker;
     EditText eText;
     EditText eText_2;
+    EditText lieu;
+    SwitchMaterial garde_chez_petsitte;
+    SwitchMaterial garde_chez_vous;
+    SwitchMaterial promenade;
+    SwitchMaterial chien;
+    SwitchMaterial chat;
     public static EditText test_reponse;
     Context ctx;
     Button boutton_rechercher;
+    int moisDebut;
+    int jourDebut;
+    int anneeDebut;
+
+    int moisFin;
+    int jourFin;
+    int anneeFin;
+
+    boolean garde_chez_petsitteSate = false;
+    boolean garde_chez_vousSate = false;
+    boolean promenadeState = false;
+    boolean chienState = false;
+    boolean chatState = false;
 
     @Nullable
     @Override
@@ -40,6 +61,12 @@ public class RechercheFragment extends Fragment {
         final View monFragmentRecherche = inflater.inflate(R.layout.fragment_recherche, container, false);
         ctx = monFragmentRecherche.getContext();
         eText = (EditText) monFragmentRecherche.findViewById(R.id.editText1);
+        garde_chez_petsitte = monFragmentRecherche.findViewById(R.id.garde_chez_petsitter);
+        garde_chez_vous = monFragmentRecherche.findViewById(R.id.garde_chez_vous);
+        promenade = monFragmentRecherche.findViewById(R.id.promenade);
+        chien = monFragmentRecherche.findViewById(R.id.chien);
+        chat = monFragmentRecherche.findViewById(R.id.chat);
+        lieu = monFragmentRecherche.findViewById(R.id.lieu);
         eText.setInputType(InputType.TYPE_NULL);
         eText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +80,9 @@ public class RechercheFragment extends Fragment {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                moisDebut = monthOfYear;
+                                jourDebut = dayOfMonth;
+                                anneeDebut = year;
                                 eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
                             }
                         }, year, month, day);
@@ -75,6 +105,9 @@ public class RechercheFragment extends Fragment {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                moisFin = monthOfYear;
+                                jourFin = dayOfMonth;
+                                anneeFin = year;
                                 eText_2.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
                             }
                         }, year, month, day);
@@ -84,25 +117,55 @@ public class RechercheFragment extends Fragment {
             }
         });
 
-//        public void getJson (){
-//
-//            String json:
-//            try {
-//                InputStream is = getAssets();
-//
-//            } catch (Exception e) {
-//
-//                e.printStackTrace();
-//            }
-//        }
+        garde_chez_petsitte.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                garde_chez_petsitteSate = isChecked;
+            }
+        });
+
+        garde_chez_vous.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                garde_chez_vousSate = isChecked;
+            }
+        });
+
+        promenade.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                promenadeState = isChecked;
+            }
+        });
+
+        chien.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                chienState = isChecked;
+            }
+        });
+
+        chat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                chatState = isChecked;
+            }
+        });
 
         boutton_rechercher = monFragmentRecherche.findViewById(R.id.btn_rechercher);
 
         boutton_rechercher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(ctx, CardPetSitter.class);
-//                startActivity(intent);
+
+                if(garde_chez_petsitteSate != garde_chez_vousSate){
+
+                    if(garde_chez_petsitteSate )
+
+                }else{
+
+                }
+
 
 
 
