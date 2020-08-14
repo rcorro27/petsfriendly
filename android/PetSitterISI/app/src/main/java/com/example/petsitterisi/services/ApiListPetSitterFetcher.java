e15fe501f896ed8baa1541014b9c499add5ea9a4
Kodjo Michel Adjognon <kadjogno@isimtl.local>
2020-08-14 09:41:23 -0400

    boite de dialogue

--- android/PetSitterISI/app/src/main/java/com/example/petsitterisi/services/ApiListPetSitterFetcher.java
+++ android/PetSitterISI/app/src/main/java/com/example/petsitterisi/services/ApiListPetSitterFetcher.java
@@ -1,11 +1,14 @@
 package com.example.petsitterisi.services;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -53,6 +56,7 @@ public class ApiListPetSitterFetcher extends AsyncTask<String, Nullable, String>
         super.onPreExecute();
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
@@ -93,6 +97,39 @@ public class ApiListPetSitterFetcher extends AsyncTask<String, Nullable, String>
 
                             String descriptionService = sharedpreferences.getString("description_service_"+idService, null);
                             String prixService = sharedpreferences.getString("prix_service_"+idService, null);
+
+                            LinearLayout parent = new LinearLayout(context);
+                            parent.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            parent.setOrientation(LinearLayout.HORIZONTAL);
+
+                            ImageView iv = new ImageView(context);
+                            iv.setImageResource(R.drawable.image_16);
+
+                            LinearLayout enfant = new LinearLayout(context);
+                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            );
+
+                            params.setMarginStart(5);
+                            enfant.setLayoutParams(params);
+                            enfant.setOrientation(LinearLayout.VERTICAL);
+
+                            parent.addView(enfant);
+
+
+                            TextView tx1 = new TextView(context);
+                            tx1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            tx1.setText(prixService+"$");
+                            tx1.setTextColor(R.color.black);
+                            enfant.addView(tx1);
+
+
+                            TextView tx2 = new TextView(context);
+                            tx2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            tx2.setText(prixService+"$");
+                            tx2.setTextColor(R.color.black);
+                            enfant.addView(tx1);
 
 
                         }
