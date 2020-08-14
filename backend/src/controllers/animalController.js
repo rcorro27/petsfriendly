b8d0bd72b54b9480b378287b813056718e8c51c7
Samuel Perreault <samuelperreault@me.com>
2020-08-14 09:54:47 -0400

    test commit

--- backend/src/controllers/animalController.js
+++ backend/src/controllers/animalController.js
@@ -1,18 +1,28 @@
 const bd = require('../servers/bd')
 const { Animal } = require('../models/animal')
 
-// GET = req.params. pcq dans url
-// POST req.body. 
-
 //la fonction appelee par la route ajout d'animal   
 function animalAjout(req, res) {
 
-    //requete sql
-    let sql = "INSERT INTO animal (race, type_animal, poids_animal, sexe_animal, nom_animal, age_animal, url_photo_animal, tarif_supplementaire) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)"
+    let sql = "INSERT INTO animal (race, type_animal, poids_animal, sexe_animal, nom_animal, age_animal, url_photo_animal) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *"
 
     //execution de la requete
-    bd.excuterRequete(sql, [req.body.race, req.body.type_animal, req.body.poids_animal, req.body.sexe_animal, req.body.nom_animal, req.body.age_animal, req.body.url_photo_animal, req.body.tarif_supplementaire])//reqccuperer req.body
+    bd.excuterRequete(sql, [req.body.animal.race, req.body.animal.type_animal, req.body.animal.poids_animal, req.body.animal.sexe_animal, req.body.nom_animal, req.body.age_animal, req.body.url_photo_animal])//reqccuperer req.body
         .then(resultatRequete => {
+
+            let requeteSQLAnimalUtilisateur = "INSERT INTO animal_utilisateur (id_proprietaire, id_animal) VALUES ($1,$2)"
+
+            bd.excuterRequete(requeteSQLAnimalUtilisateur, [req.body.utilisateur.id, resultatRequete.rows[0].id])//id user 
+                .then(resultatRequeteAnimal => {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(resultatRequete.rows))
+                })
+                .catch(erreur => {
+                    console.error(erreur.stack)
+                    res.setHeader('Content-Type', 'text/html')
+                    res.end(erreur.stack)
+                })
+
             res.setHeader('Content-Type', 'application/json')
             res.end(JSON.stringify(resultatRequete.rows))
         })
@@ -25,10 +35,10 @@ function animalAjout(req, res) {
 
 //la fonction appelee par la route modification d'animal
 function animalModification(req, res) {
-    //########################################## A VERIFIER ###########################################//
-    let sql = "update animal set race=$1, type_animal=$2, poids_animal=$3,sexe_animal=$4, nom_animal=$5,age_animal=$6,url_photo_animal=$7,tarif_supplementaire=$8"
 
-    bd.excuterRequete(sql, [req.body.id])
+    let sql = "UPDATE animal SET race=$1, type_animal=$2, poids_animal=$3,sexe_animal=$4, nom_animal=$5,age_animal=$6,url_photo_animal=$7 WHERE id = $8"
+
+    bd.excuterRequete(sql, [req.body.race, req.body.type_animal, req.body.poids_animal, req.body.sexe_animal, req.body.nom_animal, req.body.age_animal, req.body.url_photo_animal])//mettre toutes les valeurs)
         .then(resultatRequete => {
 
             res.setHeader('Content-Type', 'application/json');
@@ -43,28 +53,15 @@ function animalModification(req, res) {
 }
 
 //la fonction appelee par la route recuperation d'animal avec l'id de l'utilisateur
-// ############################################ A FAIRE ######################################
 function AnimalRecuperationByIdUtilisateur(req, res) {
 
     //reccupere l'utilisateur
-    let sql = "select * from utilisateur where id_proprietaire = $1"
+    let sql = "SELECT * FROM animal_utilisateur WHERE id_proprietaire = $1"
 
     bd.excuterRequete(sql, [req.params.id])
         .then(resultatRequete => {
-            //verifie si utilisateur existe
-            if (resultatRequete.rowCount < 1) {
-                res.setHeader('Content-Type', 'application/json')
-                res.end(JSON.stringify({ 'erreur': 400 }))
-                return undefined
-            }
-            //avec l'id de l'utilisateur on reccupere animal_utilisateur
-            reccupererAnimalUtilisateur(req.params.id)
-                .then(resultatRequeteAnimalUtilisateur => {
-                    //verifier si un resultat existe
-
-                    res.setHeader('Content-Type', 'application/json');
-                    res.end(JSON.stringify(resultatRequete.rows))
-                })
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(resultatRequete.rows))
         })
         .catch(erreur => {
             console.error(erreur.stack)
@@ -76,19 +73,12 @@ function AnimalRecuperationByIdUtilisateur(req, res) {
 //la fonction appelee par la route recuperation d'animal avec l'id de l'animal
 function AnimalRecuperationByIdAnimal(req, res) {
 
-    let sql = "select * from animal where id = $1"
+    let sql = "SELECT * FROM animal WHERE id = $1"
 
     bd.excuterRequete(sql, [req.params.id])
         .then(resultatRequete => {
-            //verifier s'il y a un resultat
-            if (resultatRequete.rowCount >= 1) {
-                res.setHeader('Content-Type', 'application/json')
-                res.end(JSON.stringify(resultatRequete.rows))
-            }
-            else {
-                res.setHeader('Content-Type', 'application/json')
-                res.end(JSON.stringify({ "erreur": 400 }))
-            }
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(resultatRequete.rows))
         })
         .catch(erreur => {
             console.error(erreur.stack)
@@ -100,14 +90,14 @@ function AnimalRecuperationByIdAnimal(req, res) {
 //la fonction appelee par la route de suppression d'animal
 function AnimalSuppression(req, res) {
 
-    let sql = "delete from animal where id = $1"
+    let sql = "DELETE FROM animal WHERE id = $1"
 
     bd.excuterRequete(sql, [req.body.id])
         .then(resultatRequete => {
             //verifie s'il y a un resultat a supprimer
             if (resultatRequete.rowCount >= 1) {
                 res.setHeader('Content-Type', 'application/json')
-                res.end(JSON.stringify(resultatRequete.rows))
+                res.end(JSON.stringify({}))
             }
             else {
                 res.setHeader('Content-Type', 'application/json')
@@ -121,7 +111,8 @@ function AnimalSuppression(req, res) {
         })
 }
 
-// fonction pour reccuperer animal_utisateur
+/*
+ fonction pour reccuperer animal_utilisateur
 function reccupererAnimalUtilisateur(id_proprietaire) {
     return new Promise((resolve, reject) => {
         let requeteSql = "SELECT * FROM animal_utilisateur WHERE id_proprietaire=$1"
@@ -136,6 +127,8 @@ function reccupererAnimalUtilisateur(id_proprietaire) {
             })
     })
 }
+
+*/
 
 module.exports = {
     animalAjout,
