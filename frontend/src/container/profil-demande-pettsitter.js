e534ae80a30124488463e573ea7ec735dcb6ebd8
Richard Alexander Corro Chirinos <rcorroch@isimtl.local>
2020-08-14 10:00:31 -0400

    changement dans la profil demande sitter

--- frontend/src/container/profil-demande-pettsitter.js
+++ frontend/src/container/profil-demande-pettsitter.js
@@ -9,15 +9,18 @@ class ProfilDemandePettSitter extends Component {
             // ligne 21 pas la bonne syntaxe
             <div>
                 <div>
-                    <h1 className='h1'>Profile Pet Sitter</h1>
+                    <h1 className='h1 w-25 p-3 mx-auto'>Profile Pet Sitter</h1>
                 </div>
-                <div className='rounded-circle'>
+                <div className='row'>
                     <img src='../src/img/caroussel/image1.jpeg' alt='Carlos' className='img-fluid rounded-circle w-25 p-3' />
-                    <h2 className='h2'>Carlos</h2>
-                    <h6 className='h6'>Secteur de Action</h6>
-                    <input type='button' value='Contacter' />
-                    <input type='button' value='Aimer' />
-
+                    <div>
+                        <h2 className='h2'>Carlos</h2>
+                        <h6 className='h6'>Secteur de Action</h6>
+                    </div>
+                    <div>
+                        <input type='button' value='Contacter' className='btn btn-success' />
+                        <input type='button' value='Aimer' className='btn btn-danger' />
+                    </div>
                 </div>
                 <div className='w-25 p-3'>
                     <h6>Services </h6>
@@ -31,10 +34,26 @@ class ProfilDemandePettSitter extends Component {
                 </div>
                 <div className='w-100 p-3'>
                     <h6>feedback</h6>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    <div>
+                        <h3>Nom PROPRIETAIRE</h3>
+                        <h4>Date de commentaire</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </div>
+                    <div>
+                        <h3>Nom PROPRIETAIRE</h3>
+                        <h4>Date de commentaire</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </div>
+                    <div>
+                        <h6>Nom PROPRIETAIRE</h6>
+                        <h4>Date de commentaire</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </div>
+                    <div>
+                        <h6>Nom PROPRIETAIRE</h6>
+                        <h4>Date de commentaire</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </div>
                 </div>
                 <div className='w-100 p-3'>
                     <h6>Geolocalitation</h6>
