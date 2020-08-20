f89cbbc993f5abdc5901cddf837f88a4352e474c
Samuel Perreault <samuelperreault@me.com>
2020-08-20 08:48:39 -0400

    rapport journalier fix

--- backend/src/controllers/rechercheController.js
+++ backend/src/controllers/rechercheController.js
@@ -1,12 +1,9 @@
 
 
 //la fonction appelee par la route recherche de petsitters
-function recherchePetsitters(req, res)
-{
+function recherchePetsitters(req, res) {
 
 }
-
-
 
 module.exports = {
     recherchePetsitters,
