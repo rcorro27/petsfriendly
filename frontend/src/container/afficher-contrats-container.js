2a6e7548b0d900d7d05da6eee26d8f5c14c4bbf8
richard <rcorroch@isimtl.local>
2020-08-25 12:16:57 -0400

    container contrat creer

---
+++ frontend/src/container/afficher-contrats-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { withRouter } from 'react-router-dom'
+import '../css/payment.css'
+
+class AfficherContratsContainer extends Component {
+    render () {
+        return (<div />)
+    }
+}
+export default withRouter(AfficherContratsContainer)
