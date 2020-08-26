c7a1c809a6a8bca00e1b49a0e01ea0ae8a2a91da
ahmed <ahmed.meksi28@gmail.com>
2020-08-26 14:23:23 -0400

    css du profil

--- frontend/src/container/profil.js
+++ frontend/src/container/profil.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import FormData from 'form-data'
-import { Link, withRouter } from 'react-router-dom'
 
 import axios from 'axios'
 import { Button } from 'react-bootstrap'
@@ -9,8 +8,6 @@ class Profile extends Component {
     constructor () {
         super()
         this.state = {
-            user: [],
-            address: [],
             nom: '',
             urlImg: '',
             prenom: '',
@@ -31,8 +28,6 @@ class Profile extends Component {
         // const decoded = jwtdecode(token)
         if (localStorage.getItem('usertoken')) {
             this.setState({
-                user: JSON.parse(localStorage.getItem('usertoken')).utilisateur,
-                address: JSON.parse(localStorage.getItem('usertoken')).adresse,
                 nom: JSON.parse(localStorage.getItem('usertoken')).utilisateur.nom,
                 prenom: JSON.parse(localStorage.getItem('usertoken')).utilisateur.prenom,
                 age: JSON.parse(localStorage.getItem('usertoken')).utilisateur.age,
@@ -40,7 +35,7 @@ class Profile extends Component {
                 telephone: JSON.parse(localStorage.getItem('usertoken')).utilisateur.telephone,
                 est_valide: JSON.parse(localStorage.getItem('usertoken')).utilisateur.est_valide,
                 email: JSON.parse(localStorage.getItem('usertoken')).utilisateur.email,
-                urlImg: 'https://pets-friendly.herokuapp.com/images/images_profiles/' + JSON.parse(localStorage.getItem('usertoken')).utilisateur.url_photo
+                urlImg: 'http://pets-friendly.herokuapp.com/images/images_profiles/' + JSON.parse(localStorage.getItem('usertoken')).utilisateur.url_photo
             })
         } else {
             this.props.history.push('/')
@@ -86,73 +81,50 @@ class Profile extends Component {
     }
 
     render () {
-        console.log('image', this.state.urlImg)
+        console.log('state', this.state.urlImg)
         return (
             <div className='container'>
 
-                <img src={this.state.urlImg} className='img-rounded' alt='Cinque Terre' />
+                <img src={this.state.urlImg} className='imgameProfil' alt='Cinque Terre' />
                 <div className='jumbotron mt-5'>
                     <div className='col-sm-8 mx-auto'>
                         {this.state.est_valide ? <h3 className='text-center text-success '> Votre profile est active vous pouvez acceder aux services</h3> : <h3 className='text-center text-danger '>Votre profil n'est pas encore valide , vous n'avez pas l'acces a nos services</h3>}
                         <h1 className='text-center'>PROFILE </h1>
                     </div>
+                    <form method='post' encType='multipart/form-data'>
 
-                    <Link to='/update'> <Button>Update Profil</Button></Link>
-                    <Link to='/contrats'> <Button>Afficher les contrats</Button></Link>
+                        <div className='form-group'>
+                            <label htmlFor='exampleFormControlFile1'>Mettre une photo de profil</label>
+                            <input name='image' type='file' className='form-control-file' id='exampleFormControlFile1' onChange={this.fileSelected} />
+                            <Button onClick={this.fileUpload}>Upload</Button>
+                        </div>
+                    </form>
 
                     <table className='table col-md-6 mx-auto'>
                         <tbody>
                             <tr>
                                 <td>Fist Name</td>
-                                <td>{this.state.user.nom}</td>
+                                <td>{this.state.nom}</td>
                             </tr>
                             <tr>
                                 <td>Last Name</td>
-                                <td>{this.state.user.prenom}</td>
+                                <td>{this.state.prenom}</td>
                             </tr>
                             <tr>
                                 <td>Email</td>
-                                <td>{this.state.user.email}</td>
+                                <td>{this.state.email}</td>
                             </tr>
                             <tr>
                                 <td>Age</td>
-                                <td>{this.state.user.age}</td>
+                                <td>{this.state.age}</td>
                             </tr>
                             <tr>
                                 <td>Sexe</td>
-                                <td>{this.state.user.sexe}</td>
+                                <td>{this.state.sexe}</td>
                             </tr>
                             <tr>
                                 <td>Telephone</td>
-                                <td>{this.state.user.telephone}</td>
-                            </tr>
-                            <tr>
-                                <td>Numero de la rue</td>
-                                <td>{this.state.address.numero_rue}</td>
-                            </tr>
-                            <tr>
-                                <td>Nom de la rue</td>
-                                <td>{this.state.address.nom_rue}</td>
-                            </tr>
-                            <tr>
-                                <td>Code Postal</td>
-                                <td>{this.state.address.code_postal}</td>
-                            </tr>
-                            <tr>
-                                <td>Ville</td>
-                                <td>{this.state.address.ville}</td>
-                            </tr>
-                            <tr>
-                                <td>Province</td>
-                                <td>{this.state.address.province}</td>
-                            </tr>
-                            <tr>
-                                <td>Pays</td>
-                                <td>{this.state.address.pays}</td>
-                            </tr>
-                            <tr>
-                                <td>Numero appartement</td>
-                                <td>{this.state.address.numero_appt}</td>
+                                <td>{this.state.telephone}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -162,4 +134,4 @@ class Profile extends Component {
     }
 }
 
-export default withRouter(Profile)
+export default Profile
